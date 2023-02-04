f9f252f8137388395245b38549b3c4b53633976b
Euova <eyadnasr8@gmail.com>
2023-02-05 03:12:25 +0400

    Add UI

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
+const restartBtn = document.querySelector(".restart");    
+const pScore = document.querySelector(".pScore");
+const cScore = document.querySelector(".cScore"); 
+const showDecision = document.querySelector(".decision");
+const showStatus = document.querySelector(".status")
+const buttons = document.querySelectorAll('button');    
+const overlay = document.querySelector(".overlay");    
+const restartBox = document.querySelector(".endBox");
+const result = document.querySelector(".result");
+const playerChoice = document.querySelector(".playerChoice");
+const computerChoice = document.querySelector(".computerChoice");
+
+buttons.forEach(button => button.addEventListener('click',function(e){
+    const playerSelection = e.target.className;
+    const computerSelection = getComputerChoice();   
+    playRound(computerSelection,playerSelection);
+    pScore.textContent = `Player:  ${playerScore}`;
+    cScore.textContent = `Computer: ${computerScore}`;  
+    
+    if (computerScore == 5){
+        overlay.style.cssText = "display: flex; justify-content: center; align-items: center;";
+        restartBox.style.cssText = "display: block;";
+        result.textContent = "You Lost!";
+        restartBtn.addEventListener('click',restartGame);
+    }
+
+    else if (playerScore == 5){
+        overlay.style.cssText = "display: flex; justify-content: center; align-items: center;";
+        restartBox.style.cssText = "display: block;";
+        result.textContent = "You Won!";
+        restartBtn.addEventListener('click',restartGame);
+
+    }
+}));
 
 function getComputerChoice(){
     choice_number = Math.floor(Math.random()*3);
@@ -16,54 +51,88 @@ function getComputerChoice(){
 function playRound(computerSelection, playerSelection){  
 
     if (computerSelection == "rock" && playerSelection == "paper"){
-        score = score + 1;
-        return "You Win! Paper beats Rock";
+        playerChoice.textContent = "✋";
+        computerChoice.textContent = "✊";
+        showDecision.textContent = "You Win!";
+        showStatus.textContent = "Paper beats Rock";
+
+        // return "You Win! Paper beats Rock";
         
     }
     else if (computerSelection == "paper" && playerSelection == "scissors"){
-        score = score + 1;
-        return "You Win! Scissors beats Paper";
+        playerChoice.textContent = "✌";
+        computerChoice.textContent = "✋";
+        playerScore++;
+        showDecision.textContent = "You Win!";
+        showStatus.textContent = "Scissors beats Paper";
+        // return "You Win! Scissors beats Paper";
         
     }
     else if (computerSelection == "scissors" && playerSelection == "rock"){
-        score = score + 1;
-        return "You Win! Rock beats Scissors";
+        playerChoice.textContent = "✊";
+        computerChoice.textContent = "✌";
+        playerScore++;
+        showDecision.textContent = "You Win!";
+        showStatus.textContent = "Rock beats Scissors";
+        // return "You Win! Rock beats Scissors";
         
     }
 
     else if (computerSelection == "rock" && playerSelection == "rock"){
-        return "Draw! Rock with Rock";
+        playerChoice.textContent = "✊";
+        computerChoice.textContent = "✊";
+        showDecision.textContent = "Draw!";
+        showStatus.textContent  = "Rock with Rock";
+        // return "Draw! Rock with Rock";
     }    
     else if (computerSelection == "paper" && playerSelection == "paper"){
-        return "Draw! Paper with Paper";
+        playerChoice.textContent = "✋";
+        computerChoice.textContent = "✋";
+        showDecision.textContent = "Draw!";
+        showStatus.textContent  = "Paper with Paper";
+        // return "Draw! Paper with Paper";
     }
     else if (computerSelection == "scissors" && playerSelection == "scissors"){
-        return "Draw! Scissors with Scissors";
+        playerChoice.textContent = "✌";
+        computerChoice.textContent = "✌";
+        showDecision.textContent = "Draw!";
+        showStatus.textContent  = "Scissors with Scissors";
+        // return "Draw! Scissors with Scissors";
     }
 
     else if (computerSelection == "scissors" && playerSelection == "paper"){
-       return "You Lose! Scissors beats Paper";
+        playerChoice.textContent = "✋";
+        computerChoice.textContent = "✌";
+        computerScore++;
+        showDecision.textContent = "You Lose!";
+        showStatus.textContent  = "Paper beaten by Scissors";
+        // return "You Lose! Scissors beats Paper";
     }
     else if (computerSelection == "rock" && playerSelection == "scissors"){
-        return "You Lose! Rock beats Scissors";
+        playerChoice.textContent = "✌";
+        computerChoice.textContent = "✊";
+        computerScore++;
+        showDecision.textContent = "You Lose!";
+        showStatus.textContent  = "Scissors beaten by Rock";
+        // return "You Lose! Rock beats Scissors";
     }
     else if (computerSelection == "paper" && playerSelection == "rock"){
-        return "You Lose! Paper beats Rock";
-    }
-
-    else {
-        return "Try Again!";
-    }
-}
-
-function playGame(){
-    for (let i = 0; i < 5; i++){
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-        playRound();
-        console.log(playRound(computerSelection,playerSelection));
-        console.log(`Score = ${score}`);
+        playerChoice.textContent = "✊";
+        computerChoice.textContent = "✋";
+        computerScore++;
+        showDecision.textContent = "You Lose!";
+        showStatus.textContent  = "Rock beaten by Paper";
+        // return "You Lose! Paper beats Rock";
     }
 }
 
-playGame();
+function restartGame(){
+    computerScore = 0;
+    playerScore = 0;
+    overlay.style.cssText = "display: none;";
+    pScore.textContent = `Player:  ${playerScore}`;
+    cScore.textContent = `Computer: ${computerScore}`;
+    showDecision.textContent = "Rock Paper Scissors";
+}
+
+
